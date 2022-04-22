80d1150ddd4d1329ae5d945c5e7ae234d3aaf17e
RicardoMusch <54025297+RicardoMusch@users.noreply.github.com>
2022-04-22 17:44:04 +0100

    Read display_name from app settings instead of hardcoded name

--- app.py
+++ app.py
@@ -38,4 +38,4 @@ class StgkStarterApp(Application):
         menu_callback = lambda: app_payload.dialog.show_dialog(self)
 
         # now register the command with the engine
-        self.engine.register_command("Show Starter Template App...", menu_callback)
+        self.engine.register_command("Show {} App...".format(self.get_setting("display_name")), menu_callback)
